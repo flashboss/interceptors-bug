018f90b24a60e08706316af1593b638134af75bd
Luca Stancapiano <l.stancapiano@sourcesense.com>
2018-07-27 20:03:48 +0200

    first commit

--- src/main/java/it/vige/injection/interceptors/ItemServiceBean.java
+++ src/main/java/it/vige/injection/interceptors/ItemServiceBean.java
@@ -1,9 +1,7 @@
 package it.vige.injection.interceptors;
 
-import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 
-@Stateless
 public class ItemServiceBean {
 
 	@Interceptors({ OKInterceptor.class })
