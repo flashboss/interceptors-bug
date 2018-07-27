c6d1f398bb9e004b5c40c7ced581214fd0b57a55
Luca Stancapiano <l.stancapiano@sourcesense.com>
2018-07-27 22:29:48 +0200

    first commit

--- src/main/java/it/vige/injection/interceptors/OKInterceptor.java
+++ src/main/java/it/vige/injection/interceptors/OKInterceptor.java
@@ -1,16 +1,9 @@
 package it.vige.injection.interceptors;
 
-import javax.interceptor.AroundConstruct;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 
-/**
- * LoggingInterceptor binds to {@link @Logging} annotation, so methods or beans
- * which @Logging annotation is applied to, will be intercepted.
- *
- * @author Luca Stancapiano
- */
 @Interceptor
 public class OKInterceptor {
 
